0785a38348dea7568ca1f3ff26f0e03b3ba26588
Thodoris Tsironis <theogr99@gmail.com>
2026-04-22 20:04:03 +0300

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    service: { entrypoint: 'astro/assets/services/sharp' },
+    defaults: {
+      format: 'webp',
+      quality: 80,
+    },
+  },
+  fonts: [
+    {
+      name: 'Geologica',
+      cssVariable: '--font-geologica',
+      provider: fontProviders.google(),
+      weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      subsets: ['latin', 'greek'],
+      display: 'swap',
+    },
+    {
+      name: 'Alegreya',
+      cssVariable: '--font-alegreya',
+      provider: fontProviders.google(),
+      weights: [400, 500, 600, 700, 800, 900],
+      subsets: ['latin', 'greek'],
+      display: 'swap',
+    },
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
