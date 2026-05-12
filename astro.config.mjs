fb2daf88691342371a0eb1315b47b30fe88660de
Thodoris Tsironis <theogr99@gmail.com>
2026-05-12 13:23:28 +0300

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,44 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  // image: {
+  //   service: { entrypoint: 'astro/assets/services/sharp' },
+  //   defaults: {
+  //     format: 'webp',
+  //     quality: 80,
+  //   },
+  // },
   image: {
-    service: { entrypoint: 'astro/assets/services/sharp' },
-    defaults: {
-      format: 'webp',
-      quality: 80,
-    },
+    service: { entrypoint: "astro/assets/services/cloudflare" },
   },
-
   fonts: [
     {
-      name: 'Geologica',
-      cssVariable: '--font-geologica',
+      name: "Geologica",
+      cssVariable: "--font-geologica",
       provider: fontProviders.google(),
       weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      subsets: ['latin', 'greek'],
-      display: 'swap',
+      subsets: ["latin", "greek"],
+      display: "swap",
     },
     {
-      name: 'Alegreya',
-      cssVariable: '--font-alegreya',
+      name: "Alegreya",
+      cssVariable: "--font-alegreya",
       provider: fontProviders.google(),
       weights: [400, 500, 600, 700, 800, 900],
-      subsets: ['latin', 'greek'],
-      display: 'swap',
+      subsets: ["latin", "greek"],
+      display: "swap",
     },
   ],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
